99f8513a3567a08706de8c3058dd3904aebae488
Artem <kagramanov,kld@yandex.ru>
2023-08-02 13:18:08 +0400

    update dist, server, readme

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 app.use(express.static('./dist'));
 
-const routes = ['/', '/registration', '/chats', '/error', '*'];
+const routes = ['/', '/registration', '/chats', '/500', '*'];
 
 routes.forEach((route) => {
     app.get(route, (req, res) => {
