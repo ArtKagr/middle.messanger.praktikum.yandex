85d781ad3d42d9dcc45fcad4b0d9b19d237594ac
Artem <kagramanov,kld@yandex.ru>
2023-09-08 14:54:43 +0400

    fix eslint

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-    plugins: [
-        require('autoprefixer'),
-        require('postcss-nested'),
-        require('postcss-import'),
-        require('postcss-css-variables')
-    ]
+  plugins: [
+    require('autoprefixer'),
+    require('postcss-nested'),
+    require('postcss-import'),
+    require('postcss-css-variables')
+  ],
+  rules: {
+    "@typescript-eslint/ban-ts-ignore": "on"
+  }
 }
 
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -9,9 +10,9 @@ app.use(express.static('./dist'));
 const routes = ['/', '/registration', '/chats', '/500', '*'];
 
 routes.forEach((route) => {
-    app.get(route, (_, res) => {
-       res.sendFile(__dirname + '/dist/index.html');
-    });
+  app.get(route, (_, res) => {
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
+  });
 });
 
 app.listen(PORT);
