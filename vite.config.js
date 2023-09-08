eb41b7f64c11ea1168ead033de443bdf69c728f5
Artem <kagramanov,kld@yandex.ru>
2023-09-08 15:41:42 +0400

    fix eslint

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    root: resolve(__dirname, 'src'),
-    build: {
-        outDir: resolve(__dirname, 'dist')
-    }
+  root: resolve(__dirname, 'src'),
+  build: {
+    outDir: resolve(__dirname, 'dist')
+  }
 });
