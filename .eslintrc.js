d57df9945a3d9f63e65f0d56e84b3d89febcc488
Artem <kagramanov,kld@yandex.ru>
2023-09-13 10:57:34 +0400

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,10 @@ module.exports = {
     extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
+    env: {
+        browser: true,
+        node: true,
+        es6: true
+    },
     root: true,
 };
