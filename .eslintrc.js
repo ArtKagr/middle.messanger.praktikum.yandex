a333958907b5eb11fa1e1649ba590d0830316cb9
Artem <kagramanov,kld@yandex.ru>
2023-09-13 11:05:48 +0400

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
     rules: {
-        'no-unused-vars': 2,
+        'no-unused-vars': 100,
         'max-len': [1, 10000],
         'max-params': [2, 3]
     },
@@ -13,5 +13,12 @@ module.exports = {
         node: true,
         es6: true
     },
+    "@typescript-eslint/no-this-alias": [
+        'error',
+        {
+            allowDestructuring: true, // Allow `const { props, state } = this`; false by default
+            allowedNames: ["self"] // Allow `const self = this`; `[]` by default
+        }
+    ],
     root: true,
 };
