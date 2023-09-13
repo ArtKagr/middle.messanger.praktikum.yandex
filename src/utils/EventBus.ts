89c17a387912413b1358946411fbe86e22d82647
Artem <kagramanov,kld@yandex.ru>
2023-09-13 16:02:24 +0400

    fix ts

--- src/utils/EventBus.ts
+++ src/utils/EventBus.ts
@@ -1,10 +1,8 @@
-import { Callback, ObjType, ObjTypes } from '../typings';
-
-type EventBusGuard = Callback[] & ObjTypes[];
+import { Callback, ObjTypes } from '../typings';
 
 export class EventBus {
-    private listeners: Record<string, EventBusGuard> = {};
-    on(event: string, callback: Callback & ObjType) {
+    private listeners: Record<string, Callback[]> = {};
+    on(event: string, callback: Callback) {
         if (!this.listeners[event]) {
             this.listeners[event] = [];
         }
@@ -12,12 +10,12 @@ export class EventBus {
         this.listeners[event].push(callback);
     }
 
-    off(event: string, callback: Callback & ObjType): void {
+    off(event: string, callback: Callback): void {
         if (!this.listeners[event]) {
             throw new Error(`Нет события: ${event}`);
         }
 
-        this.listeners[event] = this.listeners[event].filter(listener => listener !== callback) as EventBusGuard;
+        this.listeners[event] = this.listeners[event].filter(listener => listener !== callback);
     }
 
     emit(event: string, ...args: unknown[]) {
